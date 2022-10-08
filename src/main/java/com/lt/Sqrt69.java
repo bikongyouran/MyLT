203209c9546f5f20e7f9ea7691e589e5440ab079
bikongyouran <bikongyouran@163.com>
2022-10-08 16:30:15 +0800

    69

---
+++ src/main/java/com/lt/Sqrt69.java
@@ -0,0 +1,26 @@
+package com.lt;
+
+public class Sqrt69 {
+    public static int mySqrt(int x) {
+        if (x < 0) return -1;
+        if (x == 0) return 0;
+        int left = 1, right = x;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long)mid * mid <= x) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int x = 1;
+        int res = mySqrt(x);
+        System.out.println(res);
+    }
+}
