2e8b979083d75d5fbff8fc92986b260d9f6fddb5
bikongyouran <bikongyouran@163.com>
2022-10-26 21:03:33 +0800

    udpate 24

--- src/main/java/com/lt/linkedlist/SwapNodesInPairs24.java
+++ src/main/java/com/lt/linkedlist/SwapNodesInPairs24.java
@@ -7,17 +7,41 @@ public class SwapNodesInPairs24 {
             ListNode dummy = new ListNode(-1, head);
             ListNode pre = dummy;
             ListNode cur = head;
-            ListNode tmp;
+            // format 1
+//            ListNode tmp;
+//            while (cur != null && cur.next != null) {
+//                pre.next = cur.next;
+//                tmp = cur.next.next;
+//                cur.next.next = cur;
+//                cur.next = tmp;
+//                pre = cur;
+//                cur = tmp;
+//            }
+            //format 2
             while (cur != null && cur.next != null) {
                 pre.next = cur.next;
-                tmp = cur.next.next;
-                cur.next.next = cur;
-                cur.next = tmp;
-                pre = cur;
-                cur = tmp;
+                cur.next = cur.next.next;
+                pre.next.next = cur;
+                pre = pre.next.next;
+                cur = cur.next;
             }
             return dummy.next;
         }
+
+        //递归
+//        public ListNode swapPairs(ListNode head) {
+//            // base case 退出提交
+//            if(head == null || head.next == null) return head;
+//            // 获取当前节点的下一个节点
+//            ListNode next = head.next;
+//            // 进行递归
+//            ListNode newNode = swapPairs(next.next);
+//            // 这里进行交换
+//            next.next = head;
+//            head.next = newNode;
+//
+//            return next;
+//        }
 
         int val;
         ListNode next;
