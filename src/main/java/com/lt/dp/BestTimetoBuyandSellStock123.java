c8c65692d2c289e995aa6f4d0d4695b272f119da
bikongyouran <bikongyouran@163.com>
2023-01-01 23:32:07 +0800

    update

--- src/main/java/com/lt/dp/BestTimetoBuyandSellStock123.java
+++ src/main/java/com/lt/dp/BestTimetoBuyandSellStock123.java
@@ -17,6 +17,8 @@ public class BestTimetoBuyandSellStock123 {
         }
 //        return Math.max(dp[prices.length - 1][2], Math.max(dp[prices.length - 1][4], dp[prices.length - 1][0]));
         return dp[prices.length - 1][4];
-
+// 在动态规划结束后，由于我们可以进行不超过两笔交易，因此最终的答案在 0,2,4中，且为三者中的最大值。
+// 然而我们可以发现，由于在初始条件中2,4的值已经为0，并且在状态转移的过程中我们维护的是最大值，因此2和4最终一定大于等于0。
+// 同时，如果最优的情况对应的是恰好一笔交易，那么它也会因为我们在转移时允许在同一天买入并且卖出这一宽松的条件，从2转移到4，因此最终的答案即为4.
     }
 }
