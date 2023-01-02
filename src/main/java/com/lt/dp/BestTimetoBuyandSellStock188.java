5a47165dbbb6720d8259d082586eb52714ef8c0e
bikongyouran <bikongyouran@163.com>
2023-01-02 21:08:20 +0800

    188,309

---
+++ src/main/java/com/lt/dp/BestTimetoBuyandSellStock188.java
@@ -0,0 +1,22 @@
+package com.lt.dp;
+
+public class BestTimetoBuyandSellStock188 {
+    public int maxProfit(int k, int[] prices) {
+        int[][] dp = new int[prices.length][2 * k + 1];
+        for (int i = 0; i < 2 * k + 1; i++) {
+            if (i % 2 == 1) {
+                dp[0][i] = -prices[0];
+            } else {
+                dp[0][i] = 0;
+            }
+        }
+
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+            }
+        }
+        return dp[prices.length - 1][2 * k];
+    }
+}
